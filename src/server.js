800c2038b4ce084c7371278af30514e5a4647eea
murilo <murilomendo3@gmail.com>
2025-02-17 13:35:26 -0300

    alterações no endpoint send-message

--- src/server.js
+++ src/server.js
@@ -5,20 +5,19 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Para poder processar o corpo das requisições em JSON
+app.use(express.json({ limit: '50mb' })); // Ajuste o limite para 50MB
+
 
 const sessionsDir = path.join(__dirname, 'sessions'); // Diretório onde as sessões estão armazenadas
-// Armazenar as sessões ativas e os QR Codes
-let sessions = {};
+let sessions = {}; // Armazena as sessões ativas e os QR Codes
 
 // Função para criar um cliente para um número de telefone específico
 function createClient(sessionId) {
     const client = new Client({
-        authStrategy: new LocalAuth({ clientId: sessionId, dataPath: sessionsDir }), // Usar ID único para a sessão
+        authStrategy: new LocalAuth({ clientId: sessionId, dataPath: sessionsDir }), // Usa ID único para a sessão
     });
 
     client.on('qr', (qr) => {
-        // Armazena o QR Code na sessão
         sessions[sessionId].qrCode = qr;
     });
 
@@ -31,26 +30,21 @@ function createClient(sessionId) {
     });
 
     client.initialize();
-
     return client;
 }
 
+// Carrega sessões existentes ao iniciar o servidor
 function loadExistingSessions() {
-    // Verifica se o diretório de sessões existe
     if (fs.existsSync(sessionsDir)) {
-        // Lê todos os arquivos do diretório de sessões
         const sessionFiles = fs.readdirSync(sessionsDir);
-
-        // Para cada arquivo de sessão, cria uma nova sessão
         sessionFiles.forEach((file) => {
             const sessionId = path.parse(file).name.split('-')[1];
-            const client = createClient(sessionId); // Cria e inicializa o cliente para a sessão
-            sessions[sessionId] = client; // Inicializa a estrutura da sessão
+            const client = createClient(sessionId);
+            sessions[sessionId] = client;
         });
     }
 }
 
-// Chama a função para carregar as sessões existentes ao iniciar o servidor
 loadExistingSessions();
 
 // Endpoint para iniciar uma sessão
@@ -67,10 +61,10 @@ app.get('/start-session/:sessionId', (req, res) => {
     res.status(200).json({ message: `Sessão para o número ${sessionId} iniciada.` });
 });
 
-//alteracao com envio de arquivo
+// Endpoint para enviar mensagens e arquivos
 app.post('/send-message/:sessionId', async (req, res) => {
     const { sessionId } = req.params;
-    const { para, mensagem, arquivoBase64 } = req.body;
+    const { para, nomeArquivo, arquivoBase64, mensagem } = req.body;
 
     if (!sessions[sessionId]) {
         return res.status(400).json({ message: 'Sessão não encontrada.' });
@@ -80,19 +74,22 @@ app.post('/send-message/:sessionId', async (req, res) => {
 
     console.log(`✅ Enviando mensagem com sessionId: ${sessionId}`);
     console.log(`✅ Número de destino: ${para}`);
-    console.log(`✅ Mensagem: ${mensagem}`);
+    console.log(`✅ Nome do arquivo: ${nomeArquivo || 'Nenhum'}`);
     console.log(`✅ Arquivo recebido? ${arquivoBase64 ? 'Sim' : 'Não'}`);
+    console.log(`✅ Mensagem: ${mensagem || 'Nenhuma'}`);
 
     try {
-        if (arquivoBase64) {
-            // Criar a mídia a partir do Base64
-            const media = new MessageMedia('application/pdf', arquivoBase64);
+        if (arquivoBase64 && nomeArquivo) {
+            // Criar a mídia com o nome do arquivo
+            const media = new MessageMedia('application/pdf', arquivoBase64, nomeArquivo);
 
-            // Enviar a mensagem com o arquivo
-            await client.sendMessage(`${para}@c.us`, media, { caption: mensagem });
-        } else {
+            // Enviar o arquivo sem legenda
+            await client.sendMessage(`${para}@c.us`, media);
+        } else if (mensagem) {
             // Enviar apenas a mensagem de texto
             await client.sendMessage(`${para}@c.us`, mensagem);
+        } else {
+            return res.status(400).json({ message: "Nenhuma mensagem ou arquivo enviado." });
         }
 
         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
@@ -101,29 +98,6 @@ app.post('/send-message/:sessionId', async (req, res) => {
         res.status(500).json({ message: 'Erro ao enviar mensagem.', error: error.message });
     }
 });
-
-// Endpoint para enviar mensagem
-/* app.post('/send-message/:sessionId', async (req, res) => {
-    const { sessionId } = req.params;
-    const { para, mensagem } = req.body;
-
-    if (!sessions[sessionId]) {
-        return res.status(400).json({ message: 'Sessão não encontrada.' });
-    }
-
-    const client = sessions[sessionId];
-
-    console.log(`✅ Enviando mensagem com sessionId: ${sessionId}`);
-    console.log(`✅ Número de destino: ${para}`);
-    console.log(`✅ Mensagem: ${mensagem}`);
-
-    try {
-        await client.sendMessage(`${para}@c.us`, mensagem);
-        res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao enviar mensagem.', error: error.message });
-    }
-}); */
 
 // Endpoint para obter o QR Code de uma sessão
 app.get('/get-qrcode/:sessionId', (req, res) => {
